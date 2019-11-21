8277155ef4f34fbd3cce1564b94ff269b01f84a1
carpanla <carpanla@gmail.com>
2019-11-21 18:46:55 +0100

    add javascript code

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 'use strict';
-
 const face = document.querySelector('.face');
 const eyesOpen = document.querySelector('.eyes-open');
 const eyesClose = document.querySelector('.eyes-close');
 const head = document.querySelector('.head');
 
+
 function changeEyes () {
   console.log('Holiii');
+  if (eyesOpen.innerHTML === ':'){
+    eyesOpen.innerHTML = ';';
+  } else {
+    eyesOpen.innerHTML = ':';
+  }
 }
-
-
-head.addEventListener('click', changeEyes);
